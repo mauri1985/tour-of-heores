74bb6404df4946b8bd9264be3dfe84e2e0a6ffc6
mauricio.german.hernandez <mauricio.german.hernandez@fing.edu.uy>
2021-07-09 16:50:44 -0300

    obteniendo del servidor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,16 +3,33 @@ import { HEROES } from './heroes/mock-heores';
 import { Hero } from './heroes/hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+    /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
